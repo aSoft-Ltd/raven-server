9d7df8b4d049b9fed3eb254d2b279f8d549f799a
andylamax <andylamax@programmer.net>
2023-10-16 14:55:28 +0300

    Could test registraion on the client side

--- config/src/commonMain/kotlin/raven/MailingConfiguration.kt
+++ config/src/commonMain/kotlin/raven/MailingConfiguration.kt
@@ -3,10 +3,23 @@ package raven
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.serialization.Serializable
 
+/**
+ * mail.smtp.host=smtp.sendgrid.net
+ * mail.smtp.port=465
+ * mail.smtp.user=apikey
+ * mail.smtp.password=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
+ * mail.smtp.auth=true
+ * mail.smtp.starttls.enable=true
+ * mail.smtp.debug=true
+ * mail.smtp.socketFactory.port=465
+ * mail.smtp.socketFactory.class=javax.net.ssl.SSLSocketFactory
+ * mail.smtp.socketFactory.fallback=false
+ */
 @Serializable
 class MailingConfiguration(
     val sender: String? = null,
     val host: String? = null,
+    val port: Int? = null,
     val user: String? = null,
     val password: String? = null
 ) {
@@ -18,16 +31,18 @@ class MailingConfiguration(
             s.contains(MailSender.Smtp.name, ignoreCase = true) -> SmtpMailerOptions(
                 host = host ?: throw smtpMustHave("host"),
                 user = user ?: throw smtpMustHave("user"),
+                port = port ?: 465,
                 password = password ?: throw smtpMustHave("password")
             )
 
-            else -> throw IllegalArgumentException("Unsupported mailing sender")
+            else -> throw IllegalArgumentException("Unsupported mail sender type. Available options are ${MailSender.values()}")
         }
     }
 
     private fun smtpMustHave(key: String) = IllegalArgumentException("smtp sender must have a $key configuration")
 
-    fun toMailer(scope: CoroutineScope): Mailer = when (val options = toOptions(scope)) {
+    fun toMailer(scope: CoroutineScope): Mailer? = when (val options = toOptions(scope)) {
+        null -> null
         is FlixServerMailerOptions -> FlixServerMailer(options)
         is MockMailerOptions -> MockMailer(options)
         is SmtpMailerOptions -> SmtpMailer(options)
