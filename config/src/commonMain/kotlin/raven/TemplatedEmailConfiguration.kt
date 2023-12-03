8099fc9005e909a7032ac73658d6066c4df1426a
andylamax <andylamax@programmer.net>
2023-12-03 11:43:19 +0300

    Added default logo to message

--- config/src/commonMain/kotlin/raven/TemplatedEmailConfiguration.kt
+++ config/src/commonMain/kotlin/raven/TemplatedEmailConfiguration.kt
@@ -10,19 +10,39 @@ class TemplatedEmailConfiguration(
     val subject: String? = null,
     val template: String? = null
 ) {
-    fun toOptions(service: String) = TemplatedEmailOptions(
-        address = Address(
+    fun toOptions(brand: String, domain: String, address: String, service: String) = TemplatedEmailOptions(
+        brand = brand,
+        domain = domain,
+        from = Address(
             name = name ?: service.replaceFirstChar { it.uppercaseChar() },
-            email = address ?: throw IllegalArgumentException("$service email address is required")
+            email = this.address ?: throw IllegalArgumentException("$service email address is required")
         ),
         subject = subject ?: throw IllegalArgumentException("$service subject is required"),
-        template = template(service)
+        template = template(service),
+        address = address,
     )
 
-    private fun template(service: String) : String {
+    private fun template(service: String): EmailTemplate {
         val path = template ?: throw IllegalArgumentException("$service template is required")
-        val file = File(path)
-        if(!file.exists()) throw IllegalArgumentException("$service could not location template at $template")
-        return file.readText()
+        val html = File("$path.html")
+        val plain = File("$path.txt")
+
+        return when {
+            html.exists() && plain.exists() -> EmailTemplate(html.readText(), plain.readText())
+            html.exists() && !plain.exists() -> {
+                val txt = html.readText()
+                EmailTemplate(txt, txt)
+            }
+
+            !html.exists() && plain.exists() -> {
+                val txt = plain.readText()
+                EmailTemplate(txt, txt)
+            }
+
+            else -> {
+                val message = "$service could not location template at $template.txt or $template.html"
+                throw IllegalArgumentException(message)
+            }
+        }
     }
 }
--- config/src/commonMain/kotlin/raven/TemplatedWrapperEmailConfiguration.kt
+++ config/src/commonMain/kotlin/raven/TemplatedWrapperEmailConfiguration.kt
@@ -6,5 +6,10 @@ import kotlinx.serialization.Serializable
 class TemplatedWrapperEmailConfiguration(
     val email: TemplatedEmailConfiguration? = null
 ) {
-    fun toOptions(service: String) = email?.toOptions(service)
+    fun toOptions(
+        brand: String,
+        domain: String,
+        address: String,
+        service: String
+    ) = email?.toOptions(brand, domain, address, service)
 }
